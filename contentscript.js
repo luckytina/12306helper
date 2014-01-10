8a2ccad0d89dbc5cc214d9c86f98cf0877811c08
liuwei <weiliu345@gmail.com>
2014-01-10 13:05:34 +0800

    修改提示信息

--- contentscript.js
+++ contentscript.js
@@ -327,8 +327,8 @@ location.pathname == '/otn/leftTicket/init' && (function () {
         //提示
         $('<ul style="margin-left:30px;">' +
             '<li style="list-style-type:disc">你应该提前试试此脚本，选择其他日期、其他车次，</li>' +
-            '<li style="list-style-type:disc">验证此脚本功能正常和你输入的乘客名字正确，</li>' +
-            '<li style="list-style-type:disc">听听提示音乐是什么</li>' +
+            '<li style="list-style-type:disc">验证此脚本功能正常和你输入的乘客名字正确，听听提示音乐是什么。</li>' +
+            '<li style="list-style-type:disc">记得刷新页面，检查是否处于登录状态。</li>' +
             '</ul>').appendTo(fieldset);
 
         var infoDiv = $('<div style="margin-top: 10px;"></div>').appendTo(fieldset);
